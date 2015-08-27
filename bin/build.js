0165bb73f7768fbc9f7ad3525df8f6d20d1ee394
Paul Panserrieu <paul.panserrieu@gmail.com>
2015-08-27 16:22:35 +0300

    add `watch`

--- bin/build.js
+++ bin/build.js
@@ -6,10 +6,19 @@ var sass = require('node-sass'),
     Q = require('q'),
     path = require('path'),
     format = require('util').format,
-    fs = require('fs');
+    chokidar = require('chokidar'),
+    fs = require('fs'),
 
-var moduleTmpl = "angular.module('settings', [])%s;",
-    constantTmpl = ".constant('%s', %s)";
+    watcherWWW,
+    watcherScss,
+
+    moduleTmpl = "angular.module('settings', [])%s;",
+    constantTmpl = ".constant('%s', %s)",
+    www = path.resolve(__dirname, '../www'),
+    sassSrcFolder = path.resolve(__dirname, '../scss'),
+    sassSrc = path.resolve(sassSrcFolder, 'main.scss'),
+    sassOut = path.resolve(www, 'app.css'),
+    settingsOut = path.resolve(www, 'settings.js');
 
 function transformSettings(settings, outPath) {
     var mod = '';
@@ -21,25 +30,52 @@ function transformSettings(settings, outPath) {
     return settings;
 }
 
-module.exports.build = function build(platform, localSettings, config) {
-    var defer = Q.defer(),
-        sassSrc = path.resolve(__dirname, '../scss/main.scss'),
-        sassOut = path.resolve(__dirname, '../www/app.css'),
-        settingsOut = path.resolve(__dirname, '../www/settings.js');
+function sassRenderer(src, out) {
+    var defer = Q.defer();
 
     sass.render({
-        file: sassSrc
+        file: src
     }, function(err, result) {
         if(err) defer.reject(err);
         else defer.resolve(result);
     });
+
     return defer.promise.then(function (r) {
-        fs.writeFileSync(sassOut, r.css);
+        fs.writeFileSync(out, r.css);
         console.log('- sass - done');
+    });
+}
+
+module.exports.build = function build(platform, localSettings, config) {
+    return sassRenderer(sassSrc, sassOut).then(function () {
         var conf = localSettings.configurations[platform][config];
         conf.version = conf.version || localSettings.version;
         return conf;
-    }).then(function (settings) {
-        return transformSettings(settings, settingsOut);
+    }).then(function (conf) {
+        return transformSettings(conf, settingsOut);
     });
+};
+
+module.exports.watch = function (f, localSettings, platform, config, confEmitter) {
+    watcherWWW = chokidar.watch(www, { ignored: /app\.css/, persistent: true });
+    watcherSCSS = chokidar.watch(sassSrcFolder, { persistent: true });
+
+    confEmitter.on('change', function (conf) {
+        conf.version = conf.version || localSettings.version;
+        transformSettings(conf, settingsOut);
+    });
+
+    setTimeout(function () {
+        watcherWWW.on('all', function (evt, p) { f(p); });
+        watcherSCSS.on('all', function (evt, p) {
+            sassRenderer(sassSrc, sassOut).then(function () {
+                f(sassOut);
+            });
+        });
+    }, 4000);
+};
+
+module.exports.close = function () {
+    if(watcherWWW) watcherWWW.close();
+    if(watcherSCSS) watcherSCSS.close();
 };
